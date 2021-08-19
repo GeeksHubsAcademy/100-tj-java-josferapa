319c71f39d813a37e368b8612b2f6aaf1aec2ec0
Jose Ferrús <jose.ferrus.94@gmail.com>
2021-08-19 23:11:49 +0200

    Corrección

--- src/main/java/es/geekshubs/academy/FizzBuzz.java
+++ src/main/java/es/geekshubs/academy/FizzBuzz.java
@@ -12,15 +12,15 @@ public class FizzBuzz {
     String resultado = "";
     int i;
 
-    for(i = 1; i <= count){
+    for(i = 1; i <= count; i++){
       if( i % (15) == 0){  //Va primero porque 15 es multiplo de 3 y 5, y este tiene que tener preferencia 
-        resultado += "GeekHubs";
+        resultado += "GeeksHubs";
       } else if(i % 5 == 0){
         resultado += "Hubs";
       } else if(i % 3 == 0){
         resultado += "Geeks";
       } else {
-        resultado += count;
+        resultado += i;
       }
       
       resultado += "\n";
